d9f5b2fc72c7c64e26c1bf867e0e7c93822c3676
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-05-24 20:47:38 -0400

    Better rate limiter

--- main.go
+++ main.go
@@ -5,18 +5,28 @@ package main
 import (
 	"github.com/Edouard127/lambda-rpc/internal/app/state"
 	_ "github.com/Edouard127/lambda-rpc/openapi-spec"
-	"github.com/Edouard127/lambda-rpc/pkg/api/global/middlewares"
 	v1 "github.com/Edouard127/lambda-rpc/pkg/api/v1"
 	"github.com/alexflint/go-arg"
 	"github.com/gin-gonic/gin"
+	"github.com/khaaleoo/gin-rate-limiter/core"
 	sloggin "github.com/samber/slog-gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"log/slog"
 	"os"
+	"time"
 )
 
 var _ = arg.MustParse(&state.CurrentArgs)
+var limiter = core.RateLimiter{
+	RateLimiterType: core.IPRateLimiter,
+	Key:             "iplimiter_maximum_requests_for_ip",
+	Option: core.RateLimiterOption{
+		Limit: 2,                // Maximum number of requests allowed per second
+		Burst: 5,                // Maximum number of requests allowed to burst
+		Len:   10 * time.Second, // Time window
+	},
+}
 
 // @Title Lambda RPC API
 // @Version 1.0
@@ -34,7 +44,7 @@ func main() {
 		nil),
 	)
 	router.Use(sloggin.New(logger), gin.Recovery())
-	router.Use(middlewares.RateLimit)
+	router.Use(core.RequireRateLimiter(limiter))
 
 	router.GET("/swagger/v1/*any", ginSwagger.WrapHandler(swaggerFiles.NewHandler(), ginSwagger.InstanceName("v1")))
 
