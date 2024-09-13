c493ef41b5380976b52de6d1040d7e0cf0fe67dd
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-09-13 11:04:49 -0400

    removed rate limiter args

--- pkg/cmd/args.go
+++ pkg/cmd/args.go
@@ -12,8 +12,4 @@ type Args struct {
 	PrettyJson bool       `env:"PRETTY_JSON" arg:"--pretty-json" help:"Return pretty JSON responses (CPU intensive)" default:"false"`
 
 	AllowInsecure bool `env:"ALLOW_INSECURE" arg:"--allow-insecure" help:"Allow insecure minecraft accounts to connect" default:"false"`
-
-	RateLimit    int `env:"RATE_LIMIT" arg:"--rate-limit" help:"Maximum number of requests allowed in the duration" default:"5"`
-	RateDuration int `env:"RATE_DURATION" arg:"--rate-duration" help:"Time frame in milliseconds in which requests can be made until n is reached" default:"10000"`
-	RateBurst    int `env:"RATE_BURST" arg:"--rate-burst" help:"How many requests can be handled simultaneously from one IP" default:"2"`
 }
